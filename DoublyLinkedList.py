4e26ac468ed862345c43e4781595a422aa4612b2
ABEX <abexamir@gmail.com>
2021-04-22 20:01:55 +0430

    DoublyLinkedList: Added remove_tail

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -29,24 +29,55 @@ class DoublyLinkedList:
         new_head = Node(new_value)
         current_head = self.head_node
 
-        if current_head is not None:
+        if current_head != None:
             current_head.set_prev_node(new_head)
             new_head.set_next_node(current_head)
 
         self.head_node = new_head
 
-        if self.tail_node is None:
+        if self.tail_node == None:
             self.tail_node = new_head
 
     def add_to_tail(self, new_value):
         new_tail = Node(new_value)
         current_tail = self.tail_node
 
-        if current_tail is not None:
+        if current_tail != None:
             current_tail.set_next_node(new_tail)
             new_tail.set_prev_node(current_tail)
 
         self.tail_node = new_tail
 
-        if self.head_node is None:
+        if self.head_node == None:
             self.head_node = new_tail
+
+    def remove_head(self):
+        removed_head = self.head_node
+
+        if removed_head == None:
+            return None
+
+        self.head_node = removed_head.get_next_node()
+
+        if self.head_node != None:
+            self.head_node.set_prev_node(None)
+
+        if removed_head == self.tail_node:
+            self.remove_tail()
+
+        return removed_head.get_value()
+
+    def remove_tail(self):
+        removed_tail = self.tail_node
+
+        if removed_tail is None:
+            return None
+        self.tail_node = removed_tail.get_prev_node()
+
+        if self.tail_node is not None:
+            self.tail_node.set_next_node(None)
+
+        if removed_tail == self.head_node:
+            self.remove_head()
+        return removed_tail.get_value()
+
